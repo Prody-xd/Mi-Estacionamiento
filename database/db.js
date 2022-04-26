f83919ee5cdb0de1660b896150a20ff188539163
DemonWindstrike <edua.nunezc@duocuc.cl>
2022-04-25 23:45:42 -0400

    first commit

---
+++ database/db.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'estacionamiento'
+});
+
+
+conexion.connect((error)=>{
+    if(error){
+        console.error('El error de conexion es: '+error)
+        return
+    }
+    console.log('¡Conectado a la BD MySQL!');
+})
+
+
+module.exports = conexion;
---
+++ router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const conexion = require('./database/db');
+
+router.get('/', (req, res)=>{
+    conexion.query('SELECT * FROM cliente', (error, results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    })
+})
+
+
+module.exports = router;
